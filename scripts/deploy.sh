c622fb34a2955bba2b55bbef7fd79b3a3af76f71
Christian Broberg <cb@cb-pro.local>
2019-04-03 11:35:24 +0200

    Deleted deployment destination on ORGANA - only deploys to REY

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "master" ]]; then 
 	echo
 	echo Deploying Calypso Admin App Frontend $1 ... 
-	rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/odeumcode/admin.calypso.watsonc.dk
+	# rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/odeumcode/admin.calypso.watsonc.dk
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/www/odeumcode/admin.calypso.watsonc.dk
 	echo
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Calypso Admin App MASTER updated!"}' https://hooks.slack.com/services/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
